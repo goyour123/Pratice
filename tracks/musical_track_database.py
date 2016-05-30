08264bf440b9ef6ab58354393920381a358ea7c9
Ren <goyour123@gmail.com>
2016-05-30 23:36:04 +0800

    Completed Musical Track Database

--- tracks/musical_track_database.py
+++ tracks/musical_track_database.py
@@ -89,10 +89,11 @@ for entry in all:
         VALUES ( ?, ?, ? )''',
         ( name, album_id, genre_id ) )
 
-    cur.execute('''SELECT Track.title, Artist.name, Album.title, Genre.name
-        FROM Track JOIN Genre JOIN Album JOIN Artist
-        ON Track.genre_id = Genre.ID AND Track.album_id = Album.id
-            AND Album.artist_id = Artist.id
-        ORDER BY Artist.name LIMIT 3''')
-
     conn.commit()
+
+# Run These Codes on SQLite:
+# cur.execute('''SELECT Track.title, Artist.name, Album.title, Genre.name
+#     FROM Track JOIN Genre JOIN Album JOIN Artist
+#     ON Track.genre_id = Genre.ID AND Track.album_id = Album.id
+#         AND Album.artist_id = Artist.id
+#     ORDER BY Artist.name LIMIT 3''')
