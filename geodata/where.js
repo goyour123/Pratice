7012c811ee5388acdbbd57e6a8aec7f7e38d261a
Ren Yang <goyour123@gmail.com>
2016-06-08 17:57:50 +0800

    Completed geodata

---
+++ geodata/where.js
@@ -0,0 +1,5 @@
+myData = [
+[25.7180578,-100.2794872, 'Taipei 101, Futuro Nogalar, 66484 San Nicolás de los Garza, N.L., Mexico'],
+[42.340082,-71.0894884, 'Northeastern, Boston, MA 02115, USA'],
+[38.2113643,-85.7470011, 'Bradley Ave, Louisville, KY, USA']
+];
