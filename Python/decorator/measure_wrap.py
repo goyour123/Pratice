d2c730010bffc111fe641506b5c8bda21a474b4c
goyour123 <goyour123@gmail.com>
2022-11-04 22:29:54 +0800

    Add __wrapped__ example

--- Python/decorator/measure_wrap.py
+++ Python/decorator/measure_wrap.py
@@ -16,6 +16,7 @@ def measuretime(func):
 @measuretime
 def count_down(n):
     '''Counts down from n'''
+    print (f'Counts down from {n}')
     while n > 0:
         n -= 1
 
@@ -23,3 +24,5 @@ if __name__ == '__main__':
     count_down(500000)
     print(count_down.__name__)
     print(count_down.__doc__)
+    org_count_down = count_down.__wrapped__
+    org_count_down(500000)
