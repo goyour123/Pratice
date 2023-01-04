87addbe060aa4cdcb23307563ec2fb0895f368c8
goyour123 <goyour123@gmail.com>
2023-01-04 11:48:11 +0800

    Add legend to pla.py

--- Python/ML/pla.py
+++ Python/ML/pla.py
@@ -14,39 +14,30 @@ class MplCanvas(FigureCanvasQTAgg):
         fig = Figure(figsize=(width, height), dpi=dpi, linewidth=1)
         self.axes = fig.add_subplot(211)
         self.axes2 = fig.add_subplot(212)
-        
+
         iris = datasets.load_iris()
-        iris2 = datasets.load_iris()
-        
-        sepelLen = [d[0] for d in iris['data'][:50]]
-        sepelWidth = [d[1] for d in iris['data'][:50]]
-        self.axes.scatter(sepelLen, sepelWidth, marker='x')
+        iris_classes = ['Setosa', 'Versicolour', 'Virginica']
+        marker_color = ['blue', 'red', 'green']
+        instances_num = int(len(iris['data']) / len(iris_classes))
 
-        sepelLen = [d[0] for d in iris['data'][50:100]]
-        sepelWidth = [d[1] for d in iris['data'][50:100]]
-        self.axes.scatter(sepelLen, sepelWidth, marker='o')
-
-        sepelLen = [d[0] for d in iris['data'][100:150]]
-        sepelWidth = [d[1] for d in iris['data'][100:150]]
-        self.axes.scatter(sepelLen, sepelWidth, marker='^')
+        for idx, iris_class in enumerate(iris_classes):
+            sepel_len = [d[0] for d in iris['data'][instances_num*idx:instances_num*(idx+1)]]
+            sepel_width = [d[1] for d in iris['data'][instances_num*idx:instances_num*(idx+1)]]
+            self.axes.scatter(sepel_len, sepel_width, color=marker_color[idx], marker='x', label=iris_class)
 
         self.axes.set_xlabel('Sepel Length')
         self.axes.set_ylabel('Sepel Width')
+        self.axes.legend()
 
-        sepelLen = [d[2] for d in iris['data'][:50]]
-        sepelWidth = [d[3] for d in iris['data'][:50]]
-        self.axes2.scatter(sepelLen, sepelWidth, marker='x')
 
-        sepelLen = [d[2] for d in iris['data'][50:100]]
-        sepelWidth = [d[3] for d in iris['data'][50:100]]
-        self.axes2.scatter(sepelLen, sepelWidth, marker='o')
-
-        sepelLen = [d[2] for d in iris['data'][100:150]]
-        sepelWidth = [d[3] for d in iris['data'][100:150]]
-        self.axes2.scatter(sepelLen, sepelWidth, marker='^')
+        for idx, iris_class in enumerate(iris_classes):
+            petal_len = [d[2] for d in iris['data'][instances_num*idx:instances_num*(idx+1)]]
+            petal_width = [d[3] for d in iris['data'][instances_num*idx:instances_num*(idx+1)]]
+            self.axes2.scatter(petal_len, petal_width, color=marker_color[idx], marker='x',  label=iris_class)
 
         self.axes2.set_xlabel('Petal Length')
         self.axes2.set_ylabel('Petal Width')
+        self.axes2.legend()
 
         super(MplCanvas, self).__init__(fig)
 
@@ -55,7 +46,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        sc = MplCanvas(self, width=5, height=4, dpi=100)
+        sc = MplCanvas(self, width=10, height=9, dpi=100)
         self.setCentralWidget(sc)
         self.setWindowTitle("Iris")
         self.show()
